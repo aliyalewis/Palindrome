d61864a76ed3490b76b0261904217ba7515af824
github username <github email address>
2019-10-29 14:55:01 -0700

    had to remove the for loop and remembered there is a reverse method in js.

--- index.js
+++ index.js
@@ -9,22 +9,13 @@ function palindrome(string) {
 
   let newString = array.join("");
   let newArray = newString.split("");
+  let reverse = newArray.reverse();
 
-  for (let i = 0; i < newArray.length; i++) {
-    if (newArray[i] !== newArray[newArray.length - 1]) {
-      return "Not a palindrome.";
-    } else if (newArray[i] == newArray[-1]) {
-      newArray.shift();
-      newArray.pop();
-    }
-    return "Yes, this is a palindrome!";
+  if (newString === reverse.join("")) {
+    return true;
+  } else {
+    return false;
   }
 }
 
-palindrome("almostomla");
-
-//make sure all letters are lower case.
-// split into an array.
-// check first and last elements of the array.
-// return "Yes, ${word} is a palindrome!" if true and
-// "No, ${word} is not a palindrome." if false.
+palindrome("A man, a plan, a canal. Panama");
