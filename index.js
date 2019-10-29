d0d08dfa5705f849764b2fc6882c3485b9276030
github username <github email address>
2019-10-29 13:46:43 -0700

    wrote outline of function and comments of the steps I think I'll need to test.

---
+++ index.js
@@ -0,0 +1,11 @@
+function palindrome(string) {
+    
+}
+
+palindrome("kayak")
+
+//make sure all letters are lower case.
+// split into an array.
+// check first and last elements of the array.
+// return "Yes, ${word} is a palindrome!" if true and
+// "No, ${word} is not a palindrome." if false.
